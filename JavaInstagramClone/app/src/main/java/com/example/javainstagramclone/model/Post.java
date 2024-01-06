b51564bf003bbe6ac389b53f9e5f98600ed6e282
aktasduygu <110852592+aktasduygu@users.noreply.github.com>
2024-01-06 15:17:37 +0300

    JavaInstagramClone
    For Java Instagram Clone App

---
+++ JavaInstagramClone/app/src/main/java/com/example/javainstagramclone/model/Post.java
@@ -0,0 +1,15 @@
+package com.example.javainstagramclone.model;
+
+public class Post {
+
+    public String email;
+    public String comment;
+    public String downloaderUrl;
+
+    public Post(String email, String comment, String downloaderUrl){
+        this.email=email;
+        this.comment=comment;
+        this.downloaderUrl=downloaderUrl;
+    }
+
+}
